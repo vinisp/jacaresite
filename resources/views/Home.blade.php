5604f6d7b742a828186cc9701be6126b0da28fac
vinisp <aragao93.vinicius@gmail.com>
2021-11-11 17:58:09 -0300

    teste scss

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Vinicius Aragão - Front-end - Developer</title>
+    <link rel="stylesheet" href="/app.scss">
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
