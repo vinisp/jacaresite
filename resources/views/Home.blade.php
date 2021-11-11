8482d323e82b6e2328f23d637267d29b37851ad1
vinisp <aragao93.vinicius@gmail.com>
2021-11-11 17:53:33 -0300

    estrutura basica home page

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -14,7 +14,36 @@
 </head>
 
 <body>
-    <h1> HELLO WORLD! </h1>
+    <header>
+        <nav>
+            <div>Vinicius Aragão - Front-end Developer</div>
+            <ul>
+                <ul>
+                    <li> Projetos </li>
+                    <li> Lab </li>
+                    <li> Sobre </li>
+                </ul>
+        </nav>
+        <header>
+            <section>
+                <h1> Apresentação </h1>
+                <span> Contratar </span>
+                <span> Marcar uma reunião </span>
+            </section>
+            <section>
+                <div> Card 1 </div>
+                <div> Card 2 </div>
+                <div> Card 3 </div>
+                <div> Todos Projetos </div>
+            </section>
+            <section>
+                <div> Prova social </div>
+                <span> Contratar </span>
+                <span> Marcar uma reunião </span>
+            </section>
+            <footer>
+                <div> Rodapé </div>
+            </footer>
 </body>
 
 </html>
