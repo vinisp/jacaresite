877aaa394c2ab9221dfc447688b94822fe6f9c39
vinisp <aragao93.vinicius@gmail.com>
2021-11-12 12:33:32 -0300

    come back to routes and layouts elements

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,16 +1,36 @@
 <!DOCTYPE html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <title>Testar rota</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link rel="stylesheet" type="text/css" href="/app.css">
+
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Raleway:wght@100;300;600&display=swap"
+        rel="stylesheet" type="text/css">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQFHW7T756"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-EQFHW7T756');
+    </script>
+    <script src="/app.js"></script>
+    <title>Vinicius Aragão - Front-end - Developer</title>
+
 </head>
 
 <body>
     <article>
-        <h1>Texto teste</h1>
         <?= $post ?>
     </article>
 </body>
