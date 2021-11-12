974231a59b1a2126bb9732dcc858e717e83f46dd
vinisp <aragao93.vinicius@gmail.com>
2021-11-12 12:26:10 -0300

    come back to routes and layouts elements

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
 
 Route::get('/post', function () {
     return view('post', [
-        'post' => file_get_contents(__DIR__ . '../resources/views/componets/nav.html')
+        'post' => file_get_contents(__DIR__ . '../resources/views/components/nav.html')
     ]);
 });
