600b355d1619f675217072dfc8f73b9e3d3313f5
vinisp <aragao93.vinicius@gmail.com>
2021-11-12 12:24:18 -0300

    come back to routes and layouts elements

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -45,26 +45,26 @@
                 <li> <button onclick="darkTheme()"> Tema Escuro </button> </li>
             </ul>
         </nav>
-        <header>
-            <section class="welcome-space">
-                <h1> Apresentação </h1>
-                <span> Contratar </span>
-                <span> Marcar uma reunião </span>
-            </section>
-            <section>
-                <div> Card 1 </div>
-                <div> Card 2 </div>
-                <div> Card 3 </div>
-                <div> Todos Projetos </div>
-            </section>
-            <section>
-                <div> Prova social </div>
-                <span> Contratar </span>
-                <span> Marcar uma reunião </span>
-            </section>
-            <footer>
-                <div> Rodapé </div>
-            </footer>
+    </header>
+    <section class="welcome-space">
+        <h1> Apresentação </h1>
+        <span> Contratar </span>
+        <span> Marcar uma reunião </span>
+    </section>
+    <section>
+        <div> Card 1 </div>
+        <div> Card 2 </div>
+        <div> Card 3 </div>
+        <div> Todos Projetos </div>
+    </section>
+    <section>
+        <div> Prova social </div>
+        <span> Contratar </span>
+        <span> Marcar uma reunião </span>
+    </section>
+    <footer>
+        <div> Rodapé </div>
+    </footer>
 
 
 </body>
