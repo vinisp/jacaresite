e5eb52000d29692659afb1bd4a320aa80457c345
vinisp <aragao93.vinicius@gmail.com>
2021-11-12 10:38:18 -0300

    layout pattern laravel

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,33 +1,4 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Vinicius Aragão - Front-end - Developer</title>
-    <link rel="stylesheet" href="/app.css">
-
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css2?family=Lobster&family=Raleway:wght@100;300;600&display=swap"
-        rel="stylesheet" type="text/css">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EQFHW7T756"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-
-        gtag('config', 'G-EQFHW7T756');
-    </script>
-
-</head>
-
-<body>
+<x-layout>
     <header>
         <nav class="nav-home-page">
             <div class="textLogo">viniciusAragão - Front-End Developer</div>
@@ -59,8 +30,4 @@
             <footer>
                 <div> Rodapé </div>
             </footer>
-
-            <script src="/app.js"></script>
-</body>
-
-</html>
+</x-layout>
